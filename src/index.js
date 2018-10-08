b7d9e5751250bc99bca0508dc9499fdff51e5f7d
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-10-08 23:20:44 +0200

    Fix bugé

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-export * from './libs/Exception'
-export * from './libs/ExceptionSet'
-export * from './exceptions/UnimplementedException'
-export * from './exceptions/UnreachableServerException'
-export * from './exceptions/InvalidArgumentException'
-export * from './exceptions/InvalidUrlException'
+import Exception from './libs/Exception';
+import ExceptionSet from './libs/ExceptionSet';
+import UnimplementedException from './exceptions/UnimplementedException';
+import UnreachableServerException from './exceptions/UnreachableServerException';
+import InvalidArgumentException from './exceptions/InvalidArgumentException';
+import InvalidUrlException from './exceptions/InvalidUrlException';
+
+export {
+    Exception,
+    ExceptionSet,
+    UnimplementedException,
+    UnreachableServerException,
+    InvalidArgumentException,
+    InvalidUrlException,
+};
